e20c6f89bd57974989f347864dc579d568270bab
stefan1959 <admin@petos.com.au>
2025-09-30 09:45:19 +1000

    modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ const wind = document.getElementById("wind");
 const precip = document.getElementById("precipitation");
 const weatherIconContainer = document.getElementById("weather-icon");
 const weatherIcon = weatherIconContainer.querySelector("img");
+const hourlyForcast = document.getElementById("hourly-forecast");
 
 const now = new Date();
 const options = {
@@ -39,20 +40,23 @@ async function getGeoData() {
   weatherloc.innerHTML = `${city}, ${state} ,<br> ${country}`;
   weatherDate.innerHTML = formatedDate;
 
-  url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=weather_code,temperature_2m_max,temperature_2m_min&hourly=temperature_2m,showers&current=temperature_2m,precipitation,relative_humidity_2m,wind_speed_10m,apparent_temperature,weather_code&timezone=Australia%2FSydney`;
+  url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=weather_code,temperature_2m_max,temperature_2m_min&hourly=temperature_2m,weather_code&current=temperature_2m,precipitation,relative_humidity_2m,wind_speed_10m,apparent_temperature,weather_code&timezone=Australia%2FSydney`;
   let y = await fetchResult(url);
+  // console.log(y);
   let feelsLike = y.current.apparent_temperature;
   let currentTemp = y.current.temperature_2m;
   let relHumidity = y.current.relative_humidity_2m;
   let curWind = y.current.wind_speed_10m;
   let curPrecip = y.current.precipitation;
-  weatherIcon.src = getWeatherFileName(y.current.weather_code);
+  let fileSrc = getWeatherFileName(y.current.weather_code);
+  weatherIcon.src = fileSrc;
   feelLike.innerHTML = `${feelsLike}c`;
   weathertemp.innerHTML = `${currentTemp}&#176;`;
   humidity.innerHTML = `${relHumidity}%`;
   wind.innerHTML = `${curWind} km/h`;
   precip.innerHTML = `${curPrecip} mm`;
   displayDailyInfo(y.daily);
+  displayHourlyInfo(y.hourly);
 }
 
 async function fetchResult(url) {
@@ -133,14 +137,10 @@ function getWeatherFileName(code) {
 }
 
 function displayDailyInfo(dailyInfo) {
-  console.log(dailyInfo);
   let tempMax = dailyInfo.temperature_2m_max;
   let tempMin = dailyInfo.temperature_2m_min;
   let weatherCodes = dailyInfo.weather_code;
 
-  let dayShort = "";
-  let weatherFileCode = null;
-  // console.log(tempMin);
   // now.setDate(now.getDate() + 1);
   const options = { weekday: "short" };
   // console.log(now.toLocaleDateString('en-US', options));
@@ -149,21 +149,72 @@ function displayDailyInfo(dailyInfo) {
   //   // Code to be executed seven times
   //   console.log("This will run for the " + (i + 1) + " time.");
   // }
-  dailyList.innerHTML = "";
+
+  let html = "";
+  const date = new Date(now);
   for (let i = 0; i < 7; i++) {
-    dayShort = now.toLocaleDateString("en-US", options);
-    weatherFile = getWeatherFileName(weatherCodes[i]);
-    console.log(weatherFile);
-    dailyList.innerHTML += `
+    const dayShort = date.toLocaleDateString("en-US", options);
+    const weatherFile = getWeatherFileName(weatherCodes[i]);
+    const altText = `Weather icon for ${dayShort}`;
+
+    html += `
     <div class="weather__daily-forecast-item">
     <h3 class="weather__daily-forecast-day">${dayShort}</h3>
-    <img src="${weatherFile}" alt="overcast icon">
+    <img src="${weatherFile}" alt="${altText}">
     <div class="weather__daily-forecast-temps">
     <div class="weather__daily-forecast-temp-max">${tempMax[i]}&#176;</div>
     <div class="weather__daily-forecast-temp-min">${tempMin[i]}&#176;</div>
     </div>
     </div>
     `;
-    now.setDate(now.getDate() + 1);
+    date.setDate(date.getDate() + 1);
+  }
+  dailyList.innerHTML = html;
+}
+
+function displayHourlyInfo(theHour) {
+  //to do
+
+  let timeOfDay = theHour.time;
+  let tempForHour = theHour.temperature_2m;
+  let currentHour = now.getHours();
+  
+  let html = "";
+  hourlyForcast.html = "";
+  let i = -1;
+  do {
+    i++;
+    firstMatch = i;
+  } while (!timeOfDay[i].includes(currentHour + ":00"));
+  i++;
+  let weatherCodes = theHour.weather_code;
+  for ( let z= 0; z <= 7; z++ ) {
+  html += `
+   <div class="weather__hourly-forecast-item">
+            <div class="weather__hourly-forecast-time">
+              <img src="${getWeatherFileName(weatherCodes[i])}" alt="Icon for Hourly">
+              <div>${hourDisplay(i)}</div>
+            </div>
+            <div>${tempForHour[i]}&#176;</div>
+          </div>
+  
+  `;
+  i++;
+}
+  hourlyForcast.innerHTML = html;
+  // console.log(matchingIndexes);
+}
+
+function hourDisplay(h) {
+  if (h === 0) {
+    return "12AM";
+  } else if (h >= 1 && h <= 11) {
+    return h + "AM";
+  } else if (h === 12) {
+    return "12PM";
+  } else if (h >= 13 && h <= 23) {
+    return h - 12 + "PM";
+  } else {
+    return "Invalid hour"; // Handle cases outside of 0-23
   }
 }
